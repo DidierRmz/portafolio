1beaa64f6dcc9a216ec790a8414f2666ebf0efa5
DidierRmz <134162819+DidierRmz@users.noreply.github.com>
2024-01-04 21:28:54 -0500

    add portfolio files

---
+++ portafolio/KFC/script.js
@@ -0,0 +1,24 @@
+let theme = document.getElementById("idTema");
+theme.addEventListener("change",cambiarTheme);
+
+
+function cambiarTheme() {
+    console.log(theme);
+    if (theme.value == "Claro") {
+        document.body.classList.add("theme-light")
+        document.body.classList.remove("theme-dark")
+        document.body.classList.remove("theme-darkBlue")
+        localStorage.setItem("key",theme.value)
+    } else if (theme.value == "Oscuro") {
+        document.body.classList.add("theme-dark")
+        document.body.classList.remove("theme-light")
+        document.body.classList.remove("theme-darkBlue")
+        localStorage.setItem("key",theme.value)
+    } else {
+        document.body.classList.add("theme-darkBlue")
+        document.body.classList.remove("theme-light")
+        document.body.classList.remove("theme-dark")
+        localStorage.setItem("key",theme.value)
+    }
+}
+
